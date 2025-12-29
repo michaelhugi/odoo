7834de5e67bcd956674ec92285c98f95c1cad696
Michael Hugi <michael.hugi@informaticon.com>
2025-12-29 13:02:18 +0100

    asdf

--- custom_addons/my_finance_reader/__manifest__.py
+++ custom_addons/my_finance_reader/__manifest__.py
@@ -1,15 +1,19 @@
 {
-    'name': "Finanzübersicht",
-    'summary': "Liest Finanzdaten aus",
+    'name': 'Mein Finanz Cockpit',
     'version': '1.0',
-    'depends': ['base', 'account'],
-
-    # HIER DIE ÄNDERUNG:
+    'category': 'Accounting',
+    'summary': 'Schneller Liquiditäts-Check',
+    'description': """
+        Ein einfaches Tool zur Berechnung von Liquidität
+        basierend auf Odoo Buchungszeilen.
+    """,
+    'author': 'Michael Hugi',
+    'depends': ['base', 'account'],  # WICHTIG: Wir hängen vom Accounting Modul ab
     'data': [
-        'security/ir.model.access.csv',
+        'security/ir.model.access.csv', # Zugriffsrechte zuerst laden!
         'views/finance_tool_view.xml',
     ],
-
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
